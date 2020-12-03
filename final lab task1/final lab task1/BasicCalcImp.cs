b501050464b11e03aa61fb6cfd30d45ecfc6075d
unknown <Alamin>
2020-12-03 12:12:21 +0600

    lab final task1

---
+++ final lab task1/final lab task1/BasicCalcImp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_lab_task1
+{
+    class BasicCalcImp : BasicCalculatorInterface
+    {
+        public int division(int x, int y)
+        {
+            return x / y;
+        }
+        public int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+        public int sub(int x, int y)
+        {
+            return x - y;
+        }
+        public int sum(int x, int y)
+        {
+            return x + y;
+
+        }
+
+    }
+}
